9683e162de0c28e97057bc3e7a105f81b2e6d3db
ashu-ak <57582735+ashu-ak@users.noreply.github.com>
2023-08-03 14:04:52 +0530

    Add files via upload

---
+++ program1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myApp1
+{
+    internal class program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the temperature in Farenheit");
+            float temp = float.Parse(Console.ReadLine());
+            float tempInC = (temp - 32) * 5 / 9;
+            float tempInK = tempInC + 273;
+            string line = $"The Temperature in celcius is {tempInC} and the tempertaure in " +
+                $"kelvin is {tempInK} for given temperature {temp} ";
+            Console.WriteLine(line);
+        }
+    }
+}
